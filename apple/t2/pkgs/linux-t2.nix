8c9f0318319556119a6d9f1fed237fcd05ed8f6e
Zach Coyle <zach.coyle@gmail.com>
2024-03-29 14:16:55 -0400

    t2 linux -> 6.8

--- apple/t2/pkgs/linux-t2.nix
+++ apple/t2/pkgs/linux-t2.nix
@@ -1,53 +1,59 @@
-{ lib, buildLinux, fetchFromGitHub, fetchzip, runCommand
-, ... } @ args:
-
-let
+{
+  lib,
+  buildLinux,
+  fetchFromGitHub,
+  fetchzip,
+  runCommand,
+  ...
+} @ args: let
   patchRepo = fetchFromGitHub {
     owner = "t2linux";
     repo = "linux-t2-patches";
-    rev = "46dd873d1d9d12b26916790045008a91a95d0c11";
-    hash = "sha256-YGUGuzsAJFtZYjIW9d2XU4eGKNvMKCaWXqgqJn5TdeY=";
+    rev = "bdbf6166797992774138d956c66f0382ff87adae";
+    hash = "sha256-YGGGuzsAJFtZYjIW9d2XU4eGKNvMKCaWXqgqJn5TdeY=";
   };
 
-  version = "6.5";
+  version = "6.8";
   majorVersion = with lib; (elemAt (take 1 (splitVersion version)) 0);
 in
-buildLinux (args // {
-  inherit version;
+  buildLinux (args
+    // {
+      inherit version;
 
-  pname = "linux-t2";
-  # Snippet from nixpkgs
-  modDirVersion = with lib; "${concatStringsSep "." (take 3 (splitVersion "${version}.0"))}";
+      pname = "linux-t2";
+      # Snippet from nixpkgs
+      modDirVersion = with lib; "${concatStringsSep "." (take 3 (splitVersion "${version}.0"))}";
 
-  src = runCommand "patched-source" {} ''
-    cp -r ${fetchzip {
-      url = "mirror://kernel/linux/kernel/v${majorVersion}.x/linux-${version}.tar.xz";
-      hash = "sha256-qJmVSju69WcvDIbgrbtMyCi+OXUNTzNX2G+/0zwsPR4=";
-    }} $out
-    chmod -R u+w $out
-    cd $out
-    while read -r patch; do
-      echo "Applying patch $patch";
-      patch -p1 < $patch;
-    done < <(find ${patchRepo} -type f -name "*.patch" | sort)
-  '';
+      src = runCommand "patched-source" {} ''
+        cp -r ${fetchzip {
+          url = "mirror://kernel/linux/kernel/v${majorVersion}.x/linux-${version}.tar.xz";
+          hash = "sha256-qJqVSju69WcvDIbgrbtMyCi+OXUNTzNX2G+/0zwsPR4=";
+        }} $out
+        chmod -R u+w $out
+        cd $out
+        while read -r patch; do
+          echo "Applying patch $patch";
+          patch -p1 < $patch;
+        done < <(find ${patchRepo} -type f -name "*.patch" | sort)
+      '';
 
-  structuredExtraConfig = with lib.kernel; {
-    APPLE_BCE = module;
-    APPLE_GMUX = module;
-    BRCMFMAC = module;
-    BT_BCM = module;
-    BT_HCIBCM4377 = module;
-    BT_HCIUART_BCM = yes;
-    BT_HCIUART = module;
-    HID_APPLE_IBRIDGE = module;
-    HID_APPLE = module;
-    HID_APPLE_MAGIC_BACKLIGHT = module;
-    HID_APPLE_TOUCHBAR = module;
-    HID_SENSOR_ALS = module;
-    SND_PCM = module;
-    STAGING = yes;
-  };
+      structuredExtraConfig = with lib.kernel; {
+        APPLE_BCE = module;
+        APPLE_GMUX = module;
+        BRCMFMAC = module;
+        BT_BCM = module;
+        BT_HCIBCM4377 = module;
+        BT_HCIUART_BCM = yes;
+        BT_HCIUART = module;
+        HID_APPLE_IBRIDGE = module;
+        HID_APPLE = module;
+        HID_APPLE_MAGIC_BACKLIGHT = module;
+        HID_APPLE_TOUCHBAR = module;
+        HID_SENSOR_ALS = module;
+        SND_PCM = module;
+        STAGING = yes;
+      };
 
-  kernelPatches = [];
-} // (args.argsOverride or {}))
+      kernelPatches = [];
+    }
+    // (args.argsOverride or {}))
